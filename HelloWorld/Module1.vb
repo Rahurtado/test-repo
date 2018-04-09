33b4fe22829cf38bf17abec1e910903649e66045
Alex Hurtado <ahurtado@cleardemand.com>
2018-04-09 14:11:05 -0700

    HelloWorld/

---
+++ HelloWorld/Module1.vb
@@ -0,0 +1,13 @@
+﻿Module Module1
+
+    Sub Main()
+
+        Console.WriteLine("Hello World")
+        Console.Beep()
+        Console.WriteLine("But what I want to say is 'Hey bitch'")
+        Console.ReadLine()
+
+
+    End Sub
+
+End Module
