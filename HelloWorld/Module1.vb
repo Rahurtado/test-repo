43ded97301a184e6bda491cfb1f1abaa444d8e22
Alex Hurtado <ahurtado@cleardemand.com>
2018-04-12 12:58:42 -0700

    Added another line ending in the lights out second attempt.

--- HelloWorld/Module1.vb
+++ HelloWorld/Module1.vb
@@ -6,7 +6,9 @@
         Console.Beep()
         Console.WriteLine("But what I want to say is 'Hey bitch'")
         Console.Beep()
-        Console.WriteLine("BS'")
+        Console.WriteLine("'I see you, and I just want to dance with you
+Everytime they turn the lights out.'")
+        Console.ReadLine()
         Console.Beep()
         Console.ReadLine()
 
